4e6151b55b0b88a147e63162846dfc58cc6d24a9
147258369vk <45756619+147258369vk@users.noreply.github.com>
2020-03-18 17:33:39 +0530

    update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require('./config/db');
-require('./config/passportConfig')
+require('./backend/config/db');
+require('./backend/config/passportConfig')
 const express=require('express');
 const bodyparser=require('body-parser');
 const passport=require('passport');
 const cors=require('cors');
-var apiroutes=require('./routes/userRoutes');
+var apiroutes=require('./backend/routes/userRoutes');
 const path=require('path');
 
 
@@ -15,7 +15,7 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
 app.use('/',apiroutes);
 
-app.use("/",express.static(path.join(__dirname,"angular-src")));
+app.use("/",express.static(path.join(__dirname,"dist/angular-src")));
 
 app.use(function(req,res,next){
     res.header('Access-Control-Allow-Origin','*');
@@ -25,7 +25,7 @@ app.use(function(req,res,next){
 })
 
 app.use((req,res,next)=>{
-    res.sendFile(path.join(__dirname,"angular-src","index.html"));
+    res.sendFile(path.join(__dirname,"dist/angular-src/index.html"));
 });
 app.use(passport.initialize());
 
