c48d20e557adff47f466143cc6551c577512b0cb
147258369vk <45756619+147258369vk@users.noreply.github.com>
2020-03-18 17:41:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(function(req,res,next){
 })
 
 app.use((req,res,next)=>{
-    res.sendFile(path.join(__dirname,"dist/angular-src/index.html"));
+    res.sendFile(__dirname+"dist/angular-src/index.html");
 });
 app.use(passport.initialize());
 
